b36535682a959d88c3979f728ba14f677124fe9b
Bartacus88 <71856033+Bartacus88@users.noreply.github.com>
2020-12-01 19:05:04 -0500

    FTC Practice Checkin 12/1/2020

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCommandJK2019_template.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCommandJK2019_template.java
@@ -19,7 +19,7 @@ public class AutonomousCommandJK2019_template extends LinearOpMode {
 
     AutonomousStatesJK2019 runMe = new AutonomousStatesJK2019();
     AutoCommand cmd[] = {
-            new AutoCommand( AutonomousStates.AutoStates.MOVE, Drive.MoveType.REVERSE, 12, 0.2, 0,0, 2000),
+            new AutoCommand( AutonomousStates.AutoStates.MOVE, Drive.MoveType.REVERSE, 12, 0.2, 0,0, 2000), //This barely moved backwards.
             //new AutoCommand( AutonomousStates.AutoStates.MOVE, Drive.MoveType.REVERSE, 12, 0.2, 0,0, 2000),
             //new AutoCommand( AutonomousStates.AutoStates.MOVE, Drive.MoveType.FORWARD, 12, 0.2, 0,0, 500),
             new AutoCommand( AutonomousStates.AutoStates.MOVE, Drive.MoveType.PIVOTRIGHT, 90, 0.2, 0,0,2000),
